0a90a99b0c9d0c6c8d9ce57117ad07f599579062
shaisad <95139737+shaisad@users.noreply.github.com>
2023-10-20 21:39:49 +0600

    ajax to show price filterresult on same page added

--- GrocerEase/projects/views/customer/searchfilter/filteritemsbyprice.py
+++ GrocerEase/projects/views/customer/searchfilter/filteritemsbyprice.py
@@ -1,9 +1,14 @@
 from projects.imports import *
 
+
 def filteritemsbyprice(request):
     min_price = request.GET.get('min_price', 0)
     max_price = request.GET.get('max_price', 1000)  # Default values if not provided
 
     filtered_items = Item.objects.filter(itemprice__gte=min_price, itemprice__lte=max_price)
+    
+    template = loader.get_template('customer/filteritemsprice.html')  # Create a template for filtered results
+    context = {'items': filtered_items}
+    filtered_results = template.render(context)
 
-    return render(request, 'customer/filteritemsprice.html', {'items': filtered_items})
+    return HttpResponse(filtered_results)
