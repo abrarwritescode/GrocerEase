99c7ff14365d15a15f19ab519aa48df746c1eb9f
shaisad <95139737+shaisad@users.noreply.github.com>
2023-10-20 21:24:18 +0600

    price range slider added

--- GrocerEase/projects/views/customer/searchfilter/filteritemsbyprice.py
+++ GrocerEase/projects/views/customer/searchfilter/filteritemsbyprice.py
@@ -1,10 +1,9 @@
 from projects.imports import *
 
 def filteritemsbyprice(request):
-    min_price = request.GET.get('min_price')
-    max_price = request.GET.get('max_price')
-    
-    # Filter items based on price range
+    min_price = request.GET.get('min_price', 0)
+    max_price = request.GET.get('max_price', 1000)  # Default values if not provided
+
     filtered_items = Item.objects.filter(itemprice__gte=min_price, itemprice__lte=max_price)
 
     return render(request, 'customer/filteritemsprice.html', {'items': filtered_items})
