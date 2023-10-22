99660d551b62dc650080337cc27f2dcff2ed9273
shaisad <95139737+shaisad@users.noreply.github.com>
2023-10-22 13:03:38 +0600

    basic favorite backend done

--- GrocerEase/projects/views/customer/imports/customerviewsimport.py
+++ GrocerEase/projects/views/customer/imports/customerviewsimport.py
@@ -16,4 +16,6 @@ from projects.views.customer.searchfilter.searchitemcategoryseller import search
 from projects.views.customer.shopnavbar.viewshop import viewshop
 from projects.views.customer.searchfilter.filteritemsbycategory import filteritemsbycategory
 from projects.views.customer.searchfilter.filteritemsbyprice import filteritemsbyprice
-from  projects.views.customer.sellerdetails.viewselleritems import selleritems
+from projects.views.customer.sellerdetails.viewselleritems import selleritems
+from projects.views.customer.favorites.addtofavorites import addtofavorites
+from projects.views.customer.favorites.viewfavs import viewfavorites
