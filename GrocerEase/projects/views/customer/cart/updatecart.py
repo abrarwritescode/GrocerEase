c5d7a5a8dee2350ac7628f53fbf7e55686f50bd4
Lamia Alam Turna <95170568+turna62@users.noreply.github.com>
2023-10-23 15:41:14 +0600

    shaira fixed cart's quantity constraint

--- GrocerEase/projects/views/customer/cart/updatecart.py
+++ GrocerEase/projects/views/customer/cart/updatecart.py
@@ -25,28 +25,26 @@ def updatecart(request):
             current_quantity = product.itemquantity  # Get the current quantity in seller stock
 
             if current_quantity > 0:
-                if orderItem.quantity <= current_quantity:
-                    if current_quantity == 1:
+                if current_quantity == 1.00:
                         # Handle the case where the stock becomes 0
-                        product.itemquantity = 0
-                        restock_message = " It needs to be restocked."
-                    else:
+                    product.itemquantity = 0
+                    restock_message = " It needs to be restocked."
+                else:
                         # Decrease the quantity by 1
-                        product.itemquantity = current_quantity - 1
-                        restock_message = ""
+                    product.itemquantity = current_quantity - 1
+                    restock_message = ""
 
-                    product.save()
-                    orderItem.quantity = (orderItem.quantity + 1)
+            product.save()
+            orderItem.quantity = (orderItem.quantity + 1)
 
-                    added_datetime = timezone.now()  # Get the current date and time
-                    current_datetime = added_datetime + timedelta(hours=6)
-                    message = f"Your item '{product.itemtitle}' was added to a customer: {customer.customername}'s cart at {current_datetime}. Current Quantity: {product.itemquantity}. {restock_message}"
-                    Notification.objects.create(sender=customer, recipient=seller, item=product, message=message)
+            added_datetime = timezone.now()  # Get the current date and time
+            current_datetime = added_datetime + timedelta(hours=6)
+            message = f"Your item '{product.itemtitle}' was added to a customer: {customer.customername}'s cart at {current_datetime}. Current Quantity: {product.itemquantity}. {restock_message}"
+            Notification.objects.create(sender=customer, recipient=seller, item=product, message=message)
             #     else:
-            #         # Handle the case where the cart quantity equals or exceeds the available stock
-            #         # You can raise an exception or handle it as needed
+            #         
             # else:
-            #     # Handle the case where the item is out of stock
+            #     
 
         elif action == 'remove':
             seller = product.seller
