430ad9c751186cc071be3c2cc0f8caf2f3afaf73
shaisad <95139737+shaisad@users.noreply.github.com>
2024-01-18 19:33:12 +0600

    search partial string, orders sorted new, quantity add

--- GrocerEase/projects/views/seller/search/searchmyitems.py
+++ GrocerEase/projects/views/seller/search/searchmyitems.py
@@ -1,26 +1,30 @@
 from projects.imports import *
+from fuzzywuzzy import fuzz
 
 def sellersearch(request, seller_id):
     seller = Seller.objects.get(pk=seller_id)
 
     if request.method == 'GET':
         search_query = request.GET.get('search_query', '')
-   
 
-        items = Item.objects.filter(
-        Q(seller=seller),
-        Q(itemtitle__icontains=search_query) | Q(category__categoryname__icontains=search_query)
-)
 
+        all_items = Item.objects.filter(seller=seller)
+
+        relevant_items = [
+            item for item in all_items
+            if fuzz.partial_ratio(search_query.lower(), item.itemtitle.lower()) > 70
+            or any(fuzz.partial_ratio(search_query.lower(), category.categoryname.lower()) > 70
+                   for category in item.category.all())
+        ]
 
         context = {
-            'items': items,
+            'items': relevant_items,
             'search_query': search_query,
             'seller': seller,
         }
 
         if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-            # This is an AJAX request, return the search results as HTML
+          
             return render(request, 'seller/sellersearchresult.html', context)
 
         return render(request, 'seller/searchseller.html', context)
